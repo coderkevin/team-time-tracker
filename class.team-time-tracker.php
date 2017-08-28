6867f0dac38f5e80a04a97a53b197b0802544575
Kevin Killingsworth <kevin@redfenix.com>
2017-08-27 20:52:02 -0500

    Add admin page.
    This adds the admin page for the time tracker app.

--- class.team-time-tracker.php
+++ class.team-time-tracker.php
@@ -1,9 +1,36 @@
 <?php
 
 class TeamTimeTracker {
+	private static $instance;
 
 	public static function init() {
 		error_log( 'TeamTimeTracker: init' );
+
+		$instance = new TeamTimeTracker();
+		$instance->install_hooks();
+	}
+
+	public function install_hooks() {
+		add_action( 'admin_menu', array( $this, 'attach_menu' ) );
+	}
+
+	public function attach_menu() {
+		add_menu_page(
+			__( 'Team Time Tracker', 'team-time-tracker' ),
+			__( 'Team Time Tracker', 'team-time-tracker' ),
+			'administrator',
+			'team_time_tracker',
+			array( $this, 'admin_page' ),
+			'dashicons-clock',
+			56
+		);
+	}
+
+	public function admin_page() {
+		echo( '<div class="wrap">' );
+		echo( '<h1 class="wp-heading-inline">Team Time Tracker</h1>' );
+		echo( '<p>Hello, this is the Team Time Tracker Administrative page.</p>' );
+		echo( '</div>' );
 	}
 }
 
